2ac8c2bd49e26c09b318aaf3c86b81fb650927f5
Tienten <108110186+Tienten@users.noreply.github.com>
2023-10-21 21:23:12 -0400

    change to serve, login, and style.css

--- quixlab-bootstrap-main/theme/api.py
+++ quixlab-bootstrap-main/theme/api.py
@@ -10,15 +10,61 @@ SECRET = "iwqBZa7s6pln7v8h8zRF1l3OWbTLyzsW"
 
 terra = Terra(API_KEY, DEV_ID, SECRET)
 
-widget_response = terra.generate_widget_session(
-    reference_id="Sharon",
-    providers=["GARMIN","WITHINGS","FITBIT","GOOGLE","OURA","WAHOO","PELOTON","ZWIFT","TRAININGPEAKS","FREESTYLELIBRE","DEXCOM","COROS","HUAWEI","OMRON","RENPHO","POLAR","SUUNTO","EIGHT","APPLE","CONCEPT2","WHOOP","IFIT","TEMPO","CRONOMETER","FATSECRET","NUTRACHECK","UNDERARMOUR"],
-    auth_success_redirect_url="http://localhost:8080/",
-    auth_failure_redirect_url="http://localhost:8080/page-login",
-    language="en"
+auth_resp = terra.generate_authentication_url(
+reference_id="Sharon",
+resource="GARMIN",
+auth_success_redirect_url="http://localhost:8080/",
+auth_failure_redirect_url="http://localhost:8080/page-login",
 ).get_parsed_response()
-print(widget_response)
 
+print(auth_resp)
+
+# widget_response = terra.generate_widget_session(
+#     reference_id="Sharon",
+#     providers=["GARMIN","WITHINGS","FITBIT","GOOGLE","OURA","WAHOO","PELOTON","ZWIFT","TRAININGPEAKS","FREESTYLELIBRE","DEXCOM","COROS","HUAWEI","OMRON","RENPHO","POLAR","SUUNTO","EIGHT","APPLE","CONCEPT2","WHOOP","IFIT","TEMPO","CRONOMETER","FATSECRET","NUTRACHECK","UNDERARMOUR"],
+#     auth_success_redirect_url="http://localhost:8080/",
+#     auth_failure_redirect_url="http://localhost:8080/page-login",
+#     language="en"
+# ).get_parsed_response()
+# print(widget_response)
+
+# import urllib.parse
+
+# deeplink = urllib.parse.urljoin('http://localhost:8080/')
+# print(deeplink.path())
+
+# import requests
+
+# url = "https://api.tryterra.co/v2/auth/generateAuthToken"
+
+# headers = {
+#     "accept": "application/json",
+#     "dev-id": "earlham-testing-DoaYyfwkHC",
+#     "x-api-key": "iwqBZa7s6pln7v8h8zRF1l3OWbTLyzsW"
+# }
+
+# response = requests.post(url, headers=headers)
+# print(response.json()['token'])
+
+# import requests
+
+# url = "https://api.tryterra.co/v2/auth/authenticateUser?resource=GARMIN"
+
+# payload = {
+#     "reference_id": response.json()['token'],
+#     "auth_success_redirect_url": "http://localhost:8080/",
+#     "auth_failure_redirect_url": "http://localhost:8080/page-login"
+# }
+# headers = {
+#     "accept": "application/json",
+#     "dev-id": "earlham-testing-DoaYyfwkHC",
+#     "content-type": "application/json",
+#     "x-api-key": "iwqBZa7s6pln7v8h8zRF1l3OWbTLyzsW"
+# }
+
+# response = requests.post(url, json=payload, headers=headers)
+
+# print(response.json()['auth_url'])
 
 # Create a user object
 # USER_ID = "e036976a-027d-4eb2-8b9f-22ae3afbb382"
